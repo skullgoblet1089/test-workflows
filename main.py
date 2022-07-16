a6eba6137b2ff4419ee281d3ef36e2411b620222
Gregory LaFata <lafatag@gmail.com>
2022-07-16 11:59:30 -0400

    enhance output with workflow context variables

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ local_tz = local_time.astimezone().tzinfo.tzname(local_time)
 print("Hello, world!")
 if "GITHUB_WORKFLOW" in os.environ:
     print(
-        f"GitHub workflow {os.environ['GITHUB_WORKFLOW']} \
-        triggered by {os.environ['GITHUB_EVENT_NAME']}"
+        f"GitHub workflow '{os.environ['GITHUB_WORKFLOW']}' triggered by '{os.environ['GITHUB_EVENT_NAME']}' via actor '{os.environ['GITHUB_ACTOR']}'"
     )
 print(f"local time is: {local_time} {local_tz}")
 print(f"  utc time is: {utc_time} GMT")
